194cf4ba107da0f7650beec8c32574f1b00e8cfe
Ivan Ivanov <ivan78608@gmail.com>
2024-01-17 20:35:09 +0200

    Choose Your Own Adventure This is my new project in CodeCademy I still have some futures to add on!

---
+++ Choose Your Own Adventure/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Choose_Your_Own_Adventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* THE MYSTERIOUS NOISE */
+
+            // Start by asking for the user's name:
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Welcome to our story.");
+
+            
+
+        }
+    }
+}
