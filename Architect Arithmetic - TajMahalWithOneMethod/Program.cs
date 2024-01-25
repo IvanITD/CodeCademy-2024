35bddf1d16651874c2233effa011f5e67dd918f6
Ivan Ivanov <ivan78608@gmail.com>
2024-01-25 02:13:58 +0200

    Created a one method Taj Mahal version of the task, as asked!

---
+++ Architect Arithmetic - TajMahalWithOneMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Architect_Arithmetic__TajMahalWithOneMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateTotalCost();
+        }
+
+        static void CalculateTotalCost(
+            double length = 90.5,
+            double width = 90.5,
+            double bottom = 24.0,
+            double height = 24.0
+        )
+        {
+            double rectangleArea = length * width;
+            double triangleArea = bottom * height; 
+            triangleArea *= 4;
+
+            double flooringMaterialCost = 180;
+            
+            double totalShapeArea = rectangleArea + triangleArea;
+            double totalCost = totalShapeArea * flooringMaterialCost;
+            Console.WriteLine($"The total cost of the flooring material for the Taj Mahal is {totalCost} rupee!");
+
+        }
+    
+    }
+}
