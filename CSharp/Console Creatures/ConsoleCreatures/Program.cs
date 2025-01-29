09118fc1d780d8bca3983bd98dabe181ac582727
Ivan Ivanov <ivan78608@gmail.com>
2025-01-30 01:45:16 +0200

    Solved the first project, in the C# Course, named "Console Creatures

---
+++ CSharp/Console Creatures/ConsoleCreatures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // This is ghost like art created with the help of the ASCII table
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      // The ghost's name is Ghosty
+      Console.WriteLine("Ghosty");
+    }
+  }
+}
